ff51239dd1776b38f41164ddf6d21d6823c12061
bosees <rladuddlf3@naver.com>
2021-06-01 12:17:58 +0900

    서버 v1 완료

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ require("dotenv").config();
 
 const {PORT, MONGO_URL} = process.env;
 
+//미들웨어들
 app.use(helmet());  // 보안의 용이한 미들웨어다. 서버를 구축하기 전에 무조건 곁들여주면 좋다.
 app.use(bodyParser.json());   // 사용자가 웹사이트로 전달하는 정보를 검사하는 미들웨어이다. requsest 정보에서 form 이나 json 형태로 된 body를 검사함
 app.use(bodyParser.urlencoded({extended: true})); // 사용자가 웹사이트로 전달하는 정보를 검사하는 미들웨어이다. requsest 정보에서 form 이나 json 형태로 된 body를 검사함
 app.use(cookieParser()); // 쿠키를 전달받아서 사용할수 있도록 하는 미들웨어이다. 사용자 인증같은곳에서 쿠키를 검사할때 사용하기때문이다.
 app.use(morgan("dev")); // application에서 발생하는 모든 일들을 logging 하는 미들웨어이다.
 
+//몽고디비 연동
 mongoose.connect(MONGO_URL, {
     useNewUrlParser: true,
     useFindAndModify: false,
@@ -36,13 +38,17 @@ app.use(expressSession({
     cookie:{maxAge:(3.6e+6)*24}, // 24시간 뒤 만료(자동 삭제),
 }));
 
-app.use("/user", userRouter);
-app.use("/card", cardRouter);
+app.use("/user", userRouter); // User API Route
+app.use("/card", cardRouter); // Card API Route
 
+
+//테스트 함수
 app.get("/", (req,res)=> {
     console.log(req.session);
     res.send("안녕");
 });
+
+//서버 실행 함수
 app.listen(PORT, () => {
     console.log(`localhost: ${PORT}`);
 });
